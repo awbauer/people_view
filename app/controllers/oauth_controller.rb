d19bcfdd789797509b40a982e4226bfd12992ea1
Byron Anderson <byronanderson32@gmail.com>
2013-07-10 18:35:25 -0700

    rely less on global state for oauth dance

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,5 +1,6 @@
 class OauthController < ApplicationController
   def callback
+    set_current_nation(params[:state])
     credential = current_nation.credentials.create
     credential.request_access_token!(params[:code], callback_url)
     session[:credential_id] = credential.id
@@ -8,9 +9,10 @@ class OauthController < ApplicationController
 
   def authorize
     deauthorize!
-    set_current_nation
+    set_current_nation(params[:nation_id])
     redirect_to client.auth_code.authorize_url(
-      :redirect_uri => callback_url
+      :redirect_uri => callback_url,
+      :state => params[:nation_id]
     )
   end
 
@@ -25,8 +27,8 @@ class OauthController < ApplicationController
     current_nation.client
   end
 
-  def set_current_nation
-    session[:current_nation] = params[:nation_id]
+  def set_current_nation(id)
+    session[:current_nation] = id
   end
 
   def current_nation
